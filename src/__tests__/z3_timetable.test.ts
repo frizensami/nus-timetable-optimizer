8a443a70fa1f219e1a9bce6fe8613ec26f6f9dae
Sriram Sami <frizensami@gmail.com>
2021-04-30 12:24:47 +0800

    Add soft constraint code for z3timetable

--- src/__tests__/z3_timetable.test.ts
+++ src/__tests__/z3_timetable.test.ts
@@ -6,10 +6,30 @@ test('Z3Timetable constructs initial time arrays list as expected', () => {
     expect(z3tt.timevars).toEqual(["h0", "h1", "h2", "h3"])
 })
 
-test('Z3Timetable generates expected smtlib2 for constrained values for short example', () => {
+test('Z3Timetable generates expected smtlib2 hard constraints for short example', () => {
     const z3tt = new Z3Timetable(4);
     const sc: SlotConstraint = {start_end_times: [[0, 1]], who_id: 7} // ID 7 assigned to slots 0 and 1
     z3tt.add_hard_constraint(sc);
     const z3ttStr = "(declare-fun h0 () Int)\n(assert-soft (= h0 -1) :weight 1 :id defaultval)\n(assert (= h0 7))\n";
     expect(z3tt.generateSmtlib2String()).toEqual(z3ttStr)
+})
+
+test('Z3Timetable generates expected smtlib2 choose-one constraints short example', () => {
+    const z3tt = new Z3Timetable(4);
+    const sc: SlotConstraint = {start_end_times: [[0, 1]], who_id: 7} // ID 7 assigned to slots 0 and 1
+    const sc2: SlotConstraint = {start_end_times: [[2, 3]], who_id: 8} // ID 8 assigned to slots 2 and 3
+    z3tt.add_constraints_fulfil_only_one([sc, sc2]);
+    const z3ttExpected = `(declare-fun SL_7_8 () Int)
+(assert-soft (= SL_7_8 -1) :weight 1 :id defaultval)
+(declare-fun h0 () Int)
+(assert-soft (= h0 -1) :weight 1 :id defaultval)
+(declare-fun h2 () Int)
+(assert-soft (= h2 -1) :weight 1 :id defaultval)
+(assert (or (= SL_7_8 7) (= SL_7_8 8)))
+(assert (= (= SL_7_8 7) (= h0 7)))
+(assert (= (= SL_7_8 8) (= h2 8)))
+`;
+    const z3ttReal = z3tt.generateSmtlib2String()
+    console.log(z3ttReal)
+    expect(z3ttReal).toEqual(z3ttExpected)
 })
--- src/core/z3_timetable.ts
+++ src/core/z3_timetable.ts
@@ -1,4 +1,4 @@
- const smt = require('smtlib');
+const smt = require('smtlib');
 
 const UNASSIGNED: number = -1;
 
@@ -20,7 +20,8 @@ export class Z3Timetable {
     }
 
     /**
-     * Asserts that a number of constraints have to be fulfilled all at the same time
+     *  Indicate that a block of time cannot be used for anything else.
+     *  Set all time values in the range to a fixed ID.
      * */
     add_hard_constraint(slot: SlotConstraint) {
         slot.start_end_times.forEach(([start_time, end_time], _) => {
@@ -32,6 +33,60 @@ export class Z3Timetable {
                 this.assigned_vars_set.add(timevar);
             }
         });
+    }
+
+    /**
+     *  Add a list of constraint options, exactly one of which has to be satisfied.
+        We create the slot constraints for each (AND of all non-free timeslots now).
+        To ensure that only one SlotConstraint is selected, we need to create a new variable that represent this selection:
+
+        v1: (SL78 = 7 or SL78 = 8) and (SL78 = 7 => (.. slot constraints for id 7 ..)) ...
+
+        v2: v1 Doesn't work since due to the single-direction implication, solver can assign all the RHS without triggering LHS selector
+        v2: (SL78 = 7 or SL78 = 8) and (SL78 = 7 <=> (.. slot constraints for id 7 ..)) ... [DOUBLE IMPLICATION]
+
+        v3: v2 Doesn't work since the solver just sets one of the hour values to a random number to avoid triggering the LHS condition
+        v3: v2 + add a soft constraint to all assigned variables, soft-prefer them to be marked as UNASSIGNED
+     *
+     * */
+    add_constraints_fulfil_only_one(slots: Array<SlotConstraint>) {
+        // If we are selecting between who_ids 0, 1024, and 2048, the selector variable will be named SL_0_1024_2048
+        const selector_var: string = "SL_" + slots.map((slot) => slot.who_id).join("_")
+
+        // Make sure we declare this value later to be an integer
+        this.assigned_vars_set.add(selector_var)
+
+        // Create a list of constraints for the possible values the selector can take
+        // With same example, we have SL_0_1024_2048 == 0 OR SL_0_1024_2048 == 1024 OR SL_0_1024_2048 == 2048
+        let selector_var_possible_values = slots.map((slot) => smt.Eq(selector_var, slot.who_id));
+        this.solver.assert(smt.Or(...selector_var_possible_values));
+
+        // Now, for each slot, create a double implication (equality) between the selector value and each of the constrained hours
+        slots.forEach((slot) => {
+            // Holds all the constraints, assuming this slotconstraint is selected
+            let slot_requirements: Array<any> = [];
+            slot.start_end_times.forEach(([start_time, end_time]) => {
+                // Create a constraint to be "who_id" for all the start and end times in the slot constraint
+                // If we said: for this slot, time slots h1 and h2 need to be = ID 1024, then
+                // h1 == 1024
+                // h2 == 1024
+                for (let i = start_time; i < end_time; i++) {
+                    const timevar = this.timevars[i];
+                    // Make sure we declare this timevar since we use it
+                    this.assigned_vars_set.add(timevar);
+                    // For this seletion, constraint the timevar to the who_id requested
+                    slot_requirements.push(smt.Eq(timevar, slot.who_id));
+                }
+            });
+            // With all the requirements for the slot, add an implication that if we take a particular selector value,
+            // we must pick all these slot constraints (and vice versa)
+            // This is basically : (SL_0_1024_2048 == 1024) == (h1 == 1024 AND h2 == 1024)
+            this.solver.assert(
+                smt.Eq(
+                    smt.Eq(selector_var, slot.who_id),
+                    smt.And(...slot_requirements)));
+        });
+        
     }
 
     /**
