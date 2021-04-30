ec1e2ab09b0b42daf1126f721d77c80e8f13b398
Sriram Sami <frizensami@gmail.com>
2021-04-30 13:05:04 +0800

    Add generic timetable to constraints conversion and tests for basic cases

--- src/__tests__/timetable_to_smtlib2.test.ts
+++ src/__tests__/timetable_to_smtlib2.test.ts
@@ -27,3 +27,40 @@ test("Creates smtlib2 tables correctly", () => {
     expect(converter.who_id_table).toEqual(who_id_table);
     expect(converter.reverse_who_id_table).toEqual(r_who_id_table);
 });
+
+test("Creates smtlib2 string correctly for one module with one tutorial clashing with lecture (made-up scenario)", () => {
+    // Lecture from 1030 to 1130 
+    let lesson = new Lesson("1", "Lecture", [[new Date(2018, 11, 12, 10, 30), new Date(2018, 11, 12, 11, 30)]], ["Monday"], LessonWeek.ALL);
+    // Tutorial from 0930 to 1030
+    let lesson2 = new Lesson("1", "Tutorial", [[new Date(2018, 11, 12, 9, 30), new Date(2018, 11, 12, 10, 30)]], ["Monday"], LessonWeek.ALL);
+    // Tutorial from 1030 to 1130 (shouldn't work when we solve it)
+    let lesson3 = new Lesson("2", "Tutorial", [[new Date(2018, 11, 12, 10, 30), new Date(2018, 11, 12, 11, 30)]], ["Monday"], LessonWeek.ALL);
+    let mod = new Module("CS3203", 5, [lesson, lesson2, lesson3], true);
+    let gt = new GenericTimetable([mod], 5, 10);
+
+    // Timetable of only 5 hours, starting at 8 am and ending at 10 pm on a Monday (monday since we restrict # of half hour slots)
+    const converter = new TimetableSmtlib2Converter(gt, 10, 8, 22);
+    const smtlib2str = converter.generateSmtLib2String();
+    const smtlib2str_expected = `(declare-fun SL_0 () Int)
+(assert-soft (= SL_0 -1) :weight 1 :id defaultval)
+(declare-fun h5 () Int)
+(assert-soft (= h5 -1) :weight 1 :id defaultval)
+(declare-fun h6 () Int)
+(assert-soft (= h6 -1) :weight 1 :id defaultval)
+(declare-fun SL_1024_1025 () Int)
+(assert-soft (= SL_1024_1025 -1) :weight 1 :id defaultval)
+(declare-fun h3 () Int)
+(assert-soft (= h3 -1) :weight 1 :id defaultval)
+(declare-fun h4 () Int)
+(assert-soft (= h4 -1) :weight 1 :id defaultval)
+(assert (= SL_0 0))
+(assert (= (= SL_0 0) (and (= h5 0) (= h6 0))))
+(assert (or (= SL_1024_1025 1024) (= SL_1024_1025 1025)))
+(assert (= (= SL_1024_1025 1024) (and (= h3 1024) (= h4 1024))))
+(assert (= (= SL_1024_1025 1025) (and (= h5 1025) (= h6 1025))))
+`
+    expect(smtlib2str).toEqual(smtlib2str_expected)
+
+
+});
+
--- src/core/timetable_to_smtlib2.ts
+++ src/core/timetable_to_smtlib2.ts
@@ -1,5 +1,6 @@
 import { LessonWeek, Lesson, Module, GenericTimetable } from './generic_timetable'
 import { flipRecord } from '../util/utils'
+import { Z3Timetable, SlotConstraint } from './z3_timetable'
 
 /**
  * Convert a generic timetable to a string representing smtlib2 code
@@ -9,8 +10,9 @@ export class TimetableSmtlib2Converter {
     gt: GenericTimetable;
     start_hour: number
     end_hour: number
-    who_id_table: Record<string, number>; // string in both cases is {module id}__{lesson type}__{lesson id}
-    reverse_who_id_table: Record<number, string>;
+    who_id_table: Record<string, number> // string in both cases is {module id}__{lesson type}__{lesson id}
+    reverse_who_id_table: Record<number, string>
+    z3tt: Z3Timetable
 
     constructor(timetable: GenericTimetable, total_half_hour_slots: number, day_start_hour: number, day_end_hour: number) {
         this.gt = timetable;
@@ -19,6 +21,7 @@ export class TimetableSmtlib2Converter {
         this.who_id_table = {}
         this.reverse_who_id_table = {}
         this.populate_who_id_tables()
+        this.z3tt = new Z3Timetable(total_half_hour_slots)
     }
 
     populate_who_id_tables() {
@@ -34,6 +37,82 @@ export class TimetableSmtlib2Converter {
         this.reverse_who_id_table = flipRecord(this.who_id_table);
         // console.log(this.who_id_table);
         // console.log(this.reverse_who_id_table);
+    }
+
+    generateSmtLib2String(): string {
+        this.gt.modules.forEach((mod: Module) => {
+            if (!mod.is_compulsory) {
+                throw new Error("Non compulsory modules not implemented yet!"); // TODO implement
+            } else {
+                Object.keys(mod.lessons).forEach((lessonType: string, lessontypeidx: number, _) => {
+                    const lessons_of_lessontype: Array<Lesson> = mod.lessons[lessonType];
+                    const slot_constraints: Array<SlotConstraint> = this.module_lessons_to_slotconstraints(mod, lessons_of_lessontype);
+                    this.z3tt.add_constraints_fulfil_only_one(slot_constraints);
+                });
+            }
+        })
+        const smtlib2Str = this.z3tt.generateSmtlib2String();
+        return smtlib2Str;
+    }
+
+    /**
+     * Takes all lessons of a particular type from the module and converts it into a set of slot constraints,
+     *  where only one of them need to be fulfilled
+     * */
+    module_lessons_to_slotconstraints(mod: Module, lessons: Array<Lesson>): Array<SlotConstraint> {
+        let scs: Array<SlotConstraint> = [];
+
+        lessons.forEach((lesson) => {
+            const key = [mod.module_id, lesson.lesson_type, lesson.lesson_id].join("__");
+            const who_id = this.who_id_table[key]
+            let start_end_times: Array<[number, number]> = []
+            lesson.start_end_times.forEach(([g_start_time, g_end_time], idx) => {
+                const start_time = this.generic_time_to_z3_time(g_start_time, lesson.days[idx]);
+                const end_time = this.generic_time_to_z3_time(g_end_time, lesson.days[idx]);
+                start_end_times.push([start_time, end_time]);
+            })
+            const sc: SlotConstraint = { start_end_times: start_end_times, who_id: who_id };
+            scs.push(sc)
+        })
+        return scs;
+    }
+
+    /**
+     * Converts hour and minute + day of week into an integer representing a half-hour slot in the z3 timetable:w
+     * */
+    generic_time_to_z3_time(timeval: Date, day: string): number {
+        const hour = timeval.getHours();
+        const half_hour_addon = timeval.getMinutes() == 30 ? 1 : 0;
+        // We assume lessons within start to end hour each day
+        if (hour < this.start_hour || hour > this.end_hour) {
+            throw new Error(`Lesson either starts before start_hour ${hour} < ${this.start_hour} or ends after end_hour ${hour} > ${this.end_hour}`);
+        } else {
+            const hour_index = hour - this.start_hour
+            const day_index = this.day_str_to_idx(day)
+            // hour_index * 2 (since we count half-hours)
+            // + half_hour_addon since we offset by 1 unit if it's a half hour
+            // + number of hours in a day * 2 to get number of half-hours
+            const idx = (
+                (hour_index * 2)
+                + half_hour_addon
+                + day_index * ((this.end_hour - this.start_hour) * 2)
+            )
+            return idx;
+        }
+    }
+
+    /**
+     * Simple conversion of string into a monday-index-0 number
+     * */
+    day_str_to_idx(day: string): number {
+        const day_idxs: Record<string, number> = {
+            "monday": 0,
+            "tuesday": 1,
+            "wednesday": 2,
+            "thursday": 3,
+            "friday": 4,
+        }
+        return day_idxs[day.toLowerCase()];
     }
 
 
