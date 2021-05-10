40f6768e3d5b3dd9217a2df67fc55f79f2ddc64e
Sriram Sami <frizensami@gmail.com>
2021-05-10 18:55:39 +0800

    Add google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-000000-01');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
