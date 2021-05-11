b5751ded4d1c111febaa7f3f2023cf1dabcb8276
Sriram Sami <frizensami@gmail.com>
2021-05-11 20:57:14 +0800

    Use preact instead of react

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  // plugins: [{ plugin: require('@semantic-ui-react/craco-less') }],
+    // plugins: [{ plugin: require('@semantic-ui-react/craco-less') }],
+    webpack: {
+        alias: {
+            "react": "preact/compat",
+            "react-dom": "preact/compat",
+        }
+    }
 }
